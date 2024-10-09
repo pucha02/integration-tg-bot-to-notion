50ab1a21131926c81aeaa70065d31b983d738bd0
pucha02 <puchko.v.keykey@gmail.com>
2024-10-09 17:27:54 +0300

    bot

--- botKeyKey.js
+++ botKeyKey.js
@@ -10,6 +10,8 @@ const bot = new TelegramBot(_token, { polling: true });
 
 const userStates = new Map();
 const userTexts = new Map();
+const keyProjectName = Map();
+const valueProjectName = Map();
 
 bot.on("message", startBotMassage);
 
@@ -17,25 +19,34 @@ bot.onText(/\/add/, (msg) => {
   const chatId = msg.chat.id;
   bot.sendMessage(chatId, "Увведіть назву проекту, який хочете додати");
   userStates.set(chatId, "adding_new_project");
-
 });
 
 function startBotMassage(msg) {
-  if (!msg.text.startsWith("/")&&msg.text) {
+  if (!msg.text.startsWith("/") && msg.text) {
     const chatId = msg.chat.id;
     switch (userStates.get(chatId)) {
       case "awaiting_project":
-        sendingToNotionDB(bot, msg, chatId, userTexts, userStates, dataBaseIdNotion, createPropertiesForNewPages, main)
+        sendingToNotionDB(
+          bot,
+          msg,
+          chatId,
+          userTexts,
+          userStates,
+          dataBaseIdNotion,
+          createPropertiesForNewPages,
+          main
+        );
         break;
-    
+
       case "adding_new_project":
-        bot.sendMessage(chatId, "Увведіть id таблиці");
-        userStates.set(chatId, "database_ID");
+        bot.sendMessage(chatId, "Увведіть id бази даних");
+        keyProjectName.set(chatId, msg.text);
+        userStates.set("setting_database_ID");                                                                                                                                                                                       
         break;
-      
-      case "database_ID":
-        dataBaseIdNotion[`${userTexts}`] = ""
-    
+
+      case "setting_database_ID":
+        dataBaseIdNotion[] = "";
+        bot.sendMessage(chatId, "проект додан");
       default:
         if (msg.text) {
           userTexts.set(chatId, msg.text);
@@ -46,6 +57,5 @@ function startBotMassage(msg) {
         }
         break;
     }
-    
   }
 }
