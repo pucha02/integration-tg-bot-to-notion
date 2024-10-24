52a57582f2cd45386bb1e2b94e5b3211b915981a
pucha02 <puchko.v.keykey@gmail.com>
2024-10-24 17:29:30 +0300

    adding smile 2

--- dataPersonalCabinet.js
+++ dataPersonalCabinet.js
@@ -1,7 +1,7 @@
 let personalCabinet = {
+    '01': "1200df22f09b8107ba9af0e14899d5f5",
     '04': "1200df22f09b8101a6f6e506673805a7",
     '02': "1200df22f09b81b68292cba6ff17fddf",
-    '01': "1200df22f09b8107ba9af0e14899d5f5",
     '03': "1200df22f09b81c8bf67eb2e8db10b84"
 }
 
